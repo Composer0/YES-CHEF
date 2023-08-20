c27a69a957844f86b8a1d35884e18af470ae12f5
Orion Palmer <opalmer1989@gmail.com>
2023-08-20 11:49:08 -0400

    Updates from the previous week. Updates include building of api web controller. Sample model data. Creating state management. Lastly, Initial Styling.

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿//Created using an API Controller - Empty
+
+using Bard.Server.Data;
+using Bard.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bard.Server.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class RecipeController : ControllerBase
+	{
+		[HttpPost, Route("GetRecipeIdeas")]
+		public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+		{
+			return SampleData.RecipeIdeas;
+		}
+			//A type within a type within a type. :) ActionResult is used to return data to be displayed along with an appropriate HTTP status code. In our instance we are using it to grab List<Idea> model data to be rendered and displayed as a part of our Task
+
+	}
+}
